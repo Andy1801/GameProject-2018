daea890f16c80f5e05a38aafe7955878db900597
Andy Garcia <andy89190@gmail.com>
2018-05-05 13:19:46 -0400

    Camera Movement #2: Fixed Panning borders
    Fixed the border that would show up whenever the camera would pan and end up on the same layer as the player stage. Did this by making sure the z-axis wouldn't change.

--- Turn-Based Game Project/Assets/Camera/Scripts/CameraPan.cs
+++ Turn-Based Game Project/Assets/Camera/Scripts/CameraPan.cs
@@ -10,14 +10,25 @@ using UnityEngine;
 /// 1. The speed from pressing two different buttons at the same time seem
 /// to be different from just pressing one movement key.
 /// 
-/// 2. This has to be tested for other angles in case of rotation being added.
+/// 2. This script has to be tested for other angles in case of rotation being added.
+/// 
+/// 
+/// PROBLEMS FIXED:
+/// 3. When we get the stage out of view it gets replaced with a blue soild color
+/// due to the fact that the camera isnt viewing it anymore. Desired effect is that
+/// the stage just moves into the corner smoothing not like it is being overtaken.
+/// 
+/// SOLUTIONS:
+/// 3. Make sure that when translating the camera you don't change the z-axis position.
+/// When you do that there is a chance that the camera and the stage will be overlapping
+/// on the same plane so the border becomes what ever the camera is meant to view by default.
 /// 
 /// </summary>
 
 public class CameraPan : MonoBehaviour {
 
     // Determines at what speed the camera will pan
-    public float panSpeed;
+    public float panSpeed = 50f;
 
     // The Vectors indicting the direction of the camera in a angle and not global space
     private Vector3 forward;
@@ -26,7 +37,7 @@ public class CameraPan : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        
+
         // Gets the local forward angle of the camera. Typically the z-axis (blue line in scence view)
         forward = transform.forward;
         forward = Vector3.Normalize(forward);
@@ -64,7 +75,7 @@ public class CameraPan : MonoBehaviour {
         // POSSIBLE SOLUTION to problem 1. Needs to be tested more.
         Vector3 direction = Vector3.Normalize(rightMovement + upMovement) * panSpeed * Time.deltaTime;
 
-        // Moves the camera. Look up transform.Translate() on unity API if needed.
-        transform.Translate(direction);
+        // Moves the1 camera. Look up transform.Translate() on unity API if needed.
+        transform.Translate(new Vector3(direction.x, direction.y, 0));
     }
 }
